406e73251dbf54bcefdabcf0c2f8904b146cfc77
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:54:52 +0530

    Add files via upload

---
+++ sumanddifference.java
@@ -0,0 +1,16 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+public class sumanddifference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       int number1=sc.nextInt();
+       int number2=sc.nextInt();
+       System.out.println(number1+number2);
+       System.out.println(number1-number2);
+       sc.close();
+	}
+
+}
